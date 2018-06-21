97486166b5221fb102dd91b4f29098403d80aa40
Tomas Votruba <tomas.vot@gmail.com>
2018-06-21 16:19:18 +0200

    Add ObjectIsCreatedByFactorySniffTest

---
+++ tests/Sniffs/ObjectIsCreatedByFactorySniff/Wrong/PostFactory.php
@@ -0,0 +1,7 @@
+<?php declare(strict_types=1);
+
+namespace Tests\CodingStandards\Sniffs\ObjectIsCreatedByFactorySniff\Wrong;
+
+final class PostFactory
+{
+}
---
+++ tests/Sniffs/ObjectIsCreatedByFactorySniff/Wrong/SomeController.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+namespace Tests\CodingStandards\Sniffs\ObjectIsCreatedByFactorySniff\Wrong;
+
+final class SomeController
+{
+    public function action()
+    {
+        $post = new Post;
+    }
+}
