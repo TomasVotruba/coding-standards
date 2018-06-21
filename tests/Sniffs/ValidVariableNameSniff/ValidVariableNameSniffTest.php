448940954cdc89394e22baba6b9e9ca70aa37c9d
Tomas Votruba <tomas.vot@gmail.com>
2018-06-21 16:10:03 +0200

    Add ValidVariableNameSniffTest

---
+++ tests/Sniffs/ValidVariableNameSniff/ValidVariableNameSniffTest.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+
+namespace Tests\CodingStandards\Sniffs\ValidVariableNameSniff;
+
+use Symplify\EasyCodingStandardTester\Testing\AbstractCheckerTestCase;
+
+final class ValidVariableNameSniffTest extends AbstractCheckerTestCase
+{
+    public function testWrong(): void
+    {
+        $this->doTestWrongFile(__DIR__ . '/wrong/wrong.php.inc');
+    }
+
+    protected function provideConfig(): string
+    {
+        return __DIR__ . '/config.yml';
+    }
+}
