c3bca8958f12657cbc0c9540838a9b17e6b0a53b
Tomas Votruba <tomas.vot@gmail.com>
2018-06-21 16:30:39 +0200

    decopule case

--- tests/CsFixer/OrmJoinColumnRequireNullableFixer/OrmJoinColumnRequireNullableFixerTest.php
+++ tests/CsFixer/OrmJoinColumnRequireNullableFixer/OrmJoinColumnRequireNullableFixerTest.php
@@ -11,6 +11,7 @@ final class OrmJoinColumnRequireNullableFixerTest extends AbstractCheckerTestCas
         $this->doTestWrongToFixedFile(__DIR__ . '/wrong/wrong.php.inc', __DIR__ . '/fixed/fixed.php.inc');
         $this->doTestWrongToFixedFile(__DIR__ . '/wrong/wrong2.php.inc', __DIR__ . '/fixed/fixed2.php.inc');
         $this->doTestWrongToFixedFile(__DIR__ . '/wrong/wrong3.php.inc', __DIR__ . '/fixed/fixed3.php.inc');
+        $this->doTestWrongToFixedFile(__DIR__ . '/wrong/wrong4.php.inc', __DIR__ . '/fixed/fixed4.php.inc');
     }
 
     public function testCorrect(): void
