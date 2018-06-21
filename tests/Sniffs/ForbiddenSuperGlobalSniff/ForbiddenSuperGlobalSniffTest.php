c9e3582a5dac7b7c08df7e8709198684917d3617
Tomas Votruba <tomas.vot@gmail.com>
2018-06-21 16:08:28 +0200

    phpunit: autoload tests, even PHP_CodeSniffer

---
+++ tests/Sniffs/ForbiddenSuperGlobalSniff/ForbiddenSuperGlobalSniffTest.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace Tests\CodingStandards\Sniffs\ForbiddenExitSniff;
+
+use Symplify\EasyCodingStandardTester\Testing\AbstractCheckerTestCase;
+
+final class ForbiddenExitSniffTest extends AbstractCheckerTestCase
+{
+    public function testCorrect(): void
+    {
+        $this->doTestCorrectFile(__DIR__ . '/correct/correct.php.inc');
+    }
+
+    public function testWrong(): void
+    {
+        $this->doTestWrongFile(__DIR__ . '/wrong/wrong.php.inc');
+    }
+
+    protected function provideConfig(): string
+    {
+        return __DIR__ . '/config.yml';
+    }
+}
