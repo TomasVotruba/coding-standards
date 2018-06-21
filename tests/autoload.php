6ea9e1f451f4447421f01df5cc41d01e36350b9b
Tomas Votruba <tomas.vot@gmail.com>
2018-06-21 15:09:09 +0200

    add inspiration

--- tests/autoload.php
+++ tests/autoload.php
@@ -1,4 +1,6 @@
 <?php declare(strict_types=1);
 
+// @inspiration https://github.com/Symplify/Symplify/blob/master/tests/bootstrap.php
+
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../vendor/squizlabs/php_codesniffer/autoload.php';
